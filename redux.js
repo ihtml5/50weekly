b2062a7c7d4b8ba2e61b329b3ce22b63701e1b4f
html5@mobile <phpython8@gmail.com>
2018-01-17 07:56:09 +0800

    Create redux.js

---
+++ redux.js
@@ -0,0 +1,37 @@
+{
+    login: {
+        status: '0',
+        platform: 'wx'
+    },
+    userinfo: {
+      name: 'thinkchen',
+      logo: '',
+      nick: 'cf',
+      medal: {
+          types: [],
+          num: 0,
+      },
+      lifenum: 0
+    },
+    fight: {
+        rival: {
+            name: 'richfu',
+            nick: 'richfu',
+            logo: '',
+            score: [
+                50,
+                60,
+                70,
+                80,
+                90,
+            ]
+        },
+        owner: {
+            score: [],
+            questions: [{
+                time: 6,
+                choose: 2,
+            }]
+        }
+    }
+}
